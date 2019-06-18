6ec6a5f4f85a0e46b836c6bad4500b71c581497f
xigua0404 <51934367+xigua0404@users.noreply.github.com>
2019-06-17 20:16:00 -0400

    Create CheckSentence.py

---
+++ CheckSentence.py
@@ -0,0 +1,27 @@
+import unittest
+class sentence:
+  def check_sentence(wordslist):
+    wordDict={}
+    newlist=[]
+    for w in wordslist:
+      wordDict[w]=len(w) 
+    #print wordDict
+    longestword=max(wordDict, key=wordDict.get)
+    #print wordDict[longestword],longestword
+    newlist.append(wordDict[longestword])
+    newlist.append(longestword)
+    print newlist
+    return newlist
+
+  words="The cow jumped over the moon"
+ 	#I can change it as a user input: words=input("Input a sentence with double quote:")
+  wordslist=words.split(" ")
+  #print wordslist
+  check_sentence(wordslist)
+
+#unit test
+  def test_len(self):
+    testsentence="The longest word here is aasdkfldskafhds"
+    testlist=testsentence.split(" ")
+    self.assertEqual(check_sentence(testlist),['15','aasdkfldskafhds'])
+
